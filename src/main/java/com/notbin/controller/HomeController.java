780e71a48c55e207e45811571a82b76cfe0d91e3
Echo <chuanbin0725@qq.com>
2017-01-24 09:39:28 +0800

    暂时未配置MyB框架

---
+++ src/main/java/com/notbin/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.notbin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+	@RequestMapping("/index")
+	public String Test(){
+		return "index";
+	}
+}
